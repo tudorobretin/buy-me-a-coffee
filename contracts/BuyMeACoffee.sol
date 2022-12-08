5ac105f0a9323f2c3c6e9eaaf8e4d8759463191b
Tudor <tudorobretin@gmail.com>
2022-12-08 09:50:56 +0200

    basic contract outline

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -3,6 +3,71 @@ pragma solidity ^0.8.9;
 
 // import "hardhat/console.sol";
 
-contract Lock {
-    
+contract BuyMeACoffee {
+    event NewEntry(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+
+    struct Entry {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    Entry[] entries;
+
+    address payable owner;
+
+    // The constructor only runs at deploy.
+    constructor() {
+        // Define who the owner is by looking who deployed the code.
+        owner = payable(msg.sender);
+    }
+
+    /**
+     * @dev buy coffee for contract owner
+     * @param _name name of the coffee buyer
+     * @param _message message left by buyer
+     */
+     // The memory keyword gets rid of the variables afer they've been used.
+     // You have to allow this function to be payable.
+     // Public means anyone can call this function.
+    function buyCoffee(string memory _name, string memory _message) public payable{
+        // Check if value payed is greater than 0, if not returns the string response and nothing passes beyond this line.
+        require(msg.value > 0, "Man and I thought I'm cheap." );
+
+        // Add entry to storage.
+        entries.push(Entry(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        ));
+
+        emit NewEntry(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        );
+    }
+
+    /**
+     * @dev send the entier balance stored in this contract to the owner
+     */
+    function withdrawMoney() public {
+
+    }
+
+    /**
+     * @dev retrieve all the entries received and stored on the blockchain
+     */
+    function getEntries() public {
+
+    }
+
 }
